00fbbbc99a1cb442e7f636bc319df72789fd3d9f
AleenaJacob04 <141300254+AleenaJacob04@users.noreply.github.com>
2023-09-27 11:24:13 +0530

    Add files via upload

---
+++ aleena 23.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+using namespace std;
+class rectangle{
+	private:
+		double width;
+		double length;
+	public:
+		double area();
+	    double get(){
+		cout<<"enter the width";
+		cin>>width;
+		cout<<"enter the length";
+		cin>>length;
+	}
+		
+};
+double rectangle::area(){
+	return length*width;
+	
+}
+	class cubic::pubic rectangle{
+		private:
+		double height;
+		
+public:
+ double get(){
+   cout<<"Enter the height:";
+   cin>>height;
+}
+	}; 
+double.volume(){
+	return area()*height;
+	
+}
+int main(){
+	rectangle rl;
+	rl.get();
+	cout<<rl.area()<<endl;
+	cuboid c1;
+	c1.get();
+	cout<<cl.volume();
+}
+
---
+++ aleenaa 2.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+main(){
+	float num1,num2,quot;
+	cout<<"enter the number 1 and number 2:"<<endl;
+	cin>>num1>>num2;
+	quot=num1/num2;
+	cout<<"quotient="<<quot<<endl;
+	return 0;
+	
+
+}
